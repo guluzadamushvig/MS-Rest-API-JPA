c277283c02c3f2ac091c0a6fbb1f94a8f48395d4
Mushvig <guluzadamushvig@gmail.com>
2022-10-12 03:28:09 +0400

    Added HTTP STATUS CODES

--- jpa/src/main/java/ms/test/jpa/controller/UserController.java
+++ jpa/src/main/java/ms/test/jpa/controller/UserController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import ms.test.jpa.dao.entity.User;
 import ms.test.jpa.dao.entity.UserRequestResponse;
 import ms.test.jpa.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,11 +20,18 @@ public class UserController {
     }
 
     @PostMapping("/add")
-    public void addUser(@RequestBody User user){
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addUser(@RequestBody User user, @RequestHeader() String token){
+        if (token.equals("testToken")){
         userService.addUser(user);
+    }
+        else {
+            System.out.println("Authentication failed");
+        }
     }
 
     @PutMapping("/update/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public UserRequestResponse updateUser(@RequestBody UserRequestResponse userRequestResponse,
                                           @PathVariable Long id){
         return userService.updateUser(userRequestResponse,id);
